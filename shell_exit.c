85ec5408954e385d645c3a3a7f594b920ae0d1d6
root <Nada.Ghannam19@gmail.com>
2025-11-08 11:00:51 +0000

    Handle exit built-in arguments and normalize exit status

--- shell_exit.c
+++ shell_exit.c
@@ -1,71 +1,83 @@
 #include "simple_shell.h"
 
 /**
-* shell_exit - function
-* @s: shell_t ptr
-* @nl: u8
-*
-* Return: shell_t ptr
-*/
-shell_t	*shell_exit(shell_t *s, u8 nl)
+ * shell_exit - Cleanly exit the shell
+ * @s: shell_t pointer
+ * @nl: if non-zero, print a newline before exit text
+ *
+ * Return: NULL after freeing shell resources
+ */
+shell_t *shell_exit(shell_t *s, u8 nl)
 {
-if (s == 0)
-return (0);
-if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
-{
-if (nl)
-print_string("\n");
-print_string(EXIT_TEXT);
-print_string("\n");
-}
-return (shell_free(s));
+	if (s == 0)
+		return (0);
+
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
+	{
+		if (nl)
+			print_string("\n");
+		print_string(EXIT_TEXT);
+		print_string("\n");
+	}
+
+	return (shell_free(s));
 }
 
 /**
-* shell_exit_cmd - Handle the built-in exit command
-* @s: shell data structure
-* @args: arguments passed by user
-* Return: 0 to stop shell, 1 to continue
-*/
-int shell_exit_cmd(shell_t *s, u8 **args)
+ * shell_exit_cmd - Handle the built-in "exit" command
+ * @s: shell_t pointer
+ * @args: arguments array (args[0] == "exit")
+ *
+ * Return: NULL if shell should terminate, s otherwise
+ */
+shell_t *shell_exit_cmd(shell_t *s, u8 **args)
 {
-int status = 0;
-char *arg;
+	char *arg;
+	long status = 0;
+	int i = 0;
+	int sign = 1;
 
-if (args[0] == NULL)
-return (1);
+	if (s == 0 || args == 0 || args[0] == 0)
+		return (s);
 
-if (_strcmp(args[0], (u8 *)"exit") != 0)
-return (1);
+	/* Not the "exit" command, continue normally */
+	if (_strlen(args[0]) != _strlen((u8 *)"exit"))
+		return (s);
+	if (_strcmp(args[0], (u8 *)"exit") != 0)
+		return (s);
 
-arg = (char *)args[1];
+	arg = (char *)args[1];
 
-if (arg != NULL)
-{
-int i = 0;
+	/* If argument is provided, validate and compute status code */
+	if (arg != NULL)
+	{
+		if (arg[0] == '-')
+		{
+			sign = -1;
+			i = 1;
+		}
 
-while (arg[i])
-{
-if (arg[i] < '0' || arg[i] > '9')
-{
-fprintf(stderr, "exit: Illegal number: %s\n", arg);
-*(s->exit) = 2;
-return (1);
+		for (; arg[i]; i++)
+		{
+			if (arg[i] < '0' || arg[i] > '9')
+			{
+				/* Illegal number: do not exit, just set status 2 */
+				fprintf(stderr, "exit: Illegal number: %s\n", arg);
+				if (s->exit)
+					*(s->exit) = 2;
+				return (s);
+			}
+			status = status * 10 + (arg[i] - '0');
+		}
+
+		status *= sign;
+
+		/* Normalize to 0–255 like real shells (supports >255 and negatives) */
+		if (s->exit)
+			*(s->exit) = (unsigned char)status;
+	}
+
+	/* No argument: keep current *(s->exit) (last command status) */
+	return (shell_exit(s, 0));
 }
-i++;
-}
 
-status = atoi(arg);
-status = status % 256;
-*(s->exit) = status;
-}
-else
-{
-*(s->exit) = 0;
-}
-
-shell_free(s);
-exit(*(s->exit));
-
-return (0);
-}
