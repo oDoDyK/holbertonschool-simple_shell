c806e9b0f9cac25c5132a1866d48ce66780aa24a
root <Nada.Ghannam19@gmail.com>
2025-11-09 09:36:28 +0000

    conform shell_exit and shell_logic to Betty

--- shell_logic.c
+++ shell_logic.c
@@ -1,27 +1,40 @@
 #include "simple_shell.h"
 
+/**
+* shell_prompt - function
+* @s: shell_t ptr
+*
+* Return: shell_t ptr
+*/
 shell_t *shell_prompt(shell_t *s)
 {
-if (!s)
+if (s == 0)
 return (0);
 if (isatty(STDOUT_FILENO) && isatty(STDIN_FILENO))
 print_string(PROMPT_TEXT);
 return (s);
 }
 
+/**
+* shell_exec - execute external program
+* @s: shell_t ptr
+* @path: executable path
+* @args: argv array
+*
+* Return: shell_t ptr, or NULL on fatal error
+*/
 shell_t *shell_exec(shell_t *s, u8 *path, u8 **args)
 {
-pid_t pid;
-int status;
-u8 **envp;
-u64 x;
+pid_t	pid;
+int	status;
+u8	**envp;
+u64	x;
 
-if (!s)
+if (s == 0)
 return (0);
 envp = set_consume(set_clone(s->envp));
-if (!envp)
+if (envp == 0)
 return (0);
-
 pid = fork();
 if (pid == -1)
 {
@@ -52,30 +65,48 @@ free(envp);
 return (s);
 }
 
+/**
+* shell_iter_line - handle a single command line
+* @s: shell_t ptr
+* @args: arguments array
+* @line: line number
+*
+* Return: shell_t ptr, or NULL if shell should exit
+*/
 shell_t *shell_iter_line(shell_t *s, u8 **args, u64 line)
 {
-set_t *set;
-u8 *str;
+set_t	*set;
+u8	*str;
 
-if (!s)
+if (s == 0)
 return (0);
-if (!args)
+if (args == 0)
 return (shell_free(s));
-if (!args[0])
+if (args[0] == 0)
 return (s);
 
+/* handle "exit" builtin first */
 if (shell_exit_cmd(s, args) == 0)
 return (0);
-if (!args[0])
+
+/* case: exit with illegal number -> exit_cmd cleared args[0] */
+if (args[0] == 0)
 return (s);
 
+/* from here: treat as external command */
 s->path->extra = args[0];
-set = set_filter(set_add(set_apply(set_clone(s->path),
-set_apply_path_exec), args[0]), set_filter_path_exec);
+set = set_filter(
+set_add(
+set_apply(set_clone(s->path), set_apply_path_exec),
+args[0]
+),
+set_filter_path_exec
+);
 s->path->extra = 0;
 
-if (!set)
+if (set == 0)
 return (shell_free(s));
+
 if (set->size < 1)
 {
 print_not_found(s->name, line + 1, args[0]);
@@ -84,7 +115,7 @@ print_not_found(s->name, line + 1, args[0]);
 else
 {
 str = _strdup(set->data[0]);
-if (!str)
+if (str == 0)
 return (shell_free(s));
 if (shell_exec(s, str, args) == 0)
 {
@@ -97,50 +128,62 @@ set_free(set);
 return (s);
 }
 
+/**
+* shell_iter - main loop over commands in a line
+* @s: shell_t ptr
+*
+* Return: shell_t ptr, or NULL if shell should exit
+*/
 shell_t *shell_iter(shell_t *s)
 {
-u8 **lines, **args, *input;
-u64 x, y;
-u8 flag;
+u8	**l;
+u8	**a;
+u8	*i;
+u64	x;
+u64	y;
+u8	f;
 
-input = read_line();
-if (!input)
+i = read_line();
+if (i == 0)
 return (shell_exit(s, 1));
-lines = _strsplit(input, (u8 *)";\n");
-free(input);
-if (!lines)
+l = _strsplit(i, (u8 *)";\n");
+free(i);
+if (l == 0)
 return (shell_exit(s, 1));
-
-flag = 0;
-for (x = 0; lines[x]; x++)
+f = 0;
+for (x = 0; l[x]; x++)
 {
-args = _strsplit(lines[x], (u8 *)" ");
-if (!args)
+a = _strsplit(l[x], (u8 *)" ");
+if (a == 0)
 continue;
-flag = (shell_iter_line(s, args, x) == 0);
-for (y = 0; args[y]; y++)
-free(args[y]);
-free(args);
-if (flag)
+f = (shell_iter_line(s, a, x) == 0);
+for (y = 0; a[y]; y++)
+free(a[y]);
+free(a);
+if (f)
 break;
 }
-for (x = 0; lines[x]; x++)
-free(lines[x]);
-free(lines);
-if (flag)
+for (x = 0; l[x]; x++)
+free(l[x]);
+free(l);
+if (f)
 return (0);
 return (s);
 }
 
+/**
+* shell_runtime - entry point loop
+* @s: shell_t ptr
+*
+* Return: shell_t ptr
+*/
 shell_t *shell_runtime(shell_t *s)
 {
-if (!s)
+if (s == 0)
 return (0);
 while (1)
-{
 if (shell_iter(shell_prompt(s)) == 0)
 return (0);
-}
 return (shell_free(s));
 }
 
