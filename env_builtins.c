15f68b781183f7cfdbcb218689cb7636bc02a92e
root <Nada.Ghannam19@gmail.com>
2025-11-11 14:28:43 +0000

    Fix: implemented custom _setenv and _unsetenv for built-in commands (Task 13)

--- env_builtins.c
+++ env_builtins.c
@@ -1,11 +1,15 @@
 #include "simple_shell.h"
-#include <errno.h>
-
 
 /**
- * shell_setenv_cmd - Builtin command: setenv VARIABLE VALUE
+ * shell_setenv_cmd - Builtin command to set or modify an environment variable
  * @s: shell structure
- * @args: command arguments
+ * @args: command arguments (expects: setenv VARIABLE VALUE)
+ *
+ * Description:
+ * This function adds or updates an environment variable.
+ * If the wrong number of arguments is provided, it prints
+ * the correct usage message to stderr.
+ *
  * Return: shell pointer
  */
 shell_t *shell_setenv_cmd(shell_t *s, u8 **args)
@@ -18,24 +22,32 @@ shell_t *shell_setenv_cmd(shell_t *s, u8 **args)
 	for (argc = 0; args[argc]; argc++)
 		;
 
+	/* Expect exactly 3 arguments: setenv VARIABLE VALUE */
 	if (argc != 3)
 	{
 		fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
 		return (s);
 	}
 
-	if (setenv((char *)args[1], (char *)args[2], 1) == -1)
+	/* Use custom _setenv instead of system setenv() */
+	if (_setenv((char *)args[1], (char *)args[2], 1) == -1)
 	{
-		perror("setenv");
+		fprintf(stderr, "setenv: Failed to set variable\n");
 	}
 
 	return (s);
 }
 
 /**
- * shell_unsetenv_cmd - Builtin command: unsetenv VARIABLE
+ * shell_unsetenv_cmd - Builtin command to remove an environment variable
  * @s: shell structure
- * @args: command arguments
+ * @args: command arguments (expects: unsetenv VARIABLE)
+ *
+ * Description:
+ * This function removes an environment variable from the environment.
+ * If the wrong number of arguments is provided, it prints
+ * the correct usage message to stderr.
+ *
  * Return: shell pointer
  */
 shell_t *shell_unsetenv_cmd(shell_t *s, u8 **args)
@@ -48,15 +60,17 @@ shell_t *shell_unsetenv_cmd(shell_t *s, u8 **args)
 	for (argc = 0; args[argc]; argc++)
 		;
 
+	/* Expect exactly 2 arguments: unsetenv VARIABLE */
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: unsetenv VARIABLE\n");
 		return (s);
 	}
 
-	if (unsetenv((char *)args[1]) == -1)
+	/* Use custom _unsetenv instead of system unsetenv() */
+	if (_unsetenv((char *)args[1]) == -1)
 	{
-		perror("unsetenv");
+		fprintf(stderr, "unsetenv: Failed to unset variable\n");
 	}
 
 	return (s);
