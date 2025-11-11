29b3699aeb279c3e0be846af757d17a27d3eb12f
root <Nada.Ghannam19@gmail.com>
2025-11-11 08:25:11 +0000

    add setenv/unsetenv prototypes

--- simple_shell.h
+++ simple_shell.h
@@ -8,6 +8,7 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <errno.h>
+# include <signal.h>
 
 # define i8  int8_t
 # define u8  uint8_t
@@ -17,10 +18,10 @@
 # define u64 uint64_t
 
 /**
- * struct vector_s - struct
- * @data: []u8
- * @size: u64
- * @curr: u64
+ * struct vector_s - dynamic byte buffer
+ * @data: byte array
+ * @size: total allocated
+ * @curr: current used size
 */
 typedef struct vector_s
 {
@@ -30,12 +31,12 @@ typedef struct vector_s
 }	vector_t;
 
 /**
- * struct set_s - struct
- * @data: [][]u8
- * @extra: u8 ptr
- * @size: u64
+ * struct set_s - dynamic array of strings
+ * @data: array of string pointers
+ * @extra: helper pointer
+ * @size: number of strings
 */
-typedef struct	set_s
+typedef struct set_s
 {
 	u8	**data;
 	u8	*extra;
@@ -43,12 +44,12 @@ typedef struct	set_s
 }	set_t;
 
 /**
- * struct shell_s - struct
- * @exit: int
- * @name: u8 ptr
- * @envp: set_t ptr
- * @path: set_t ptr
- */
+ * struct shell_s - shell context
+ * @exit: pointer to exit status
+ * @name: program name
+ * @envp: environment variables
+ * @path: PATH directories
+*/
 typedef struct shell_s
 {
 	int	*exit;
@@ -63,16 +64,20 @@ u64		_strlen(u8 *);
 i32		_strcmp(u8 *, u8 *);
 u8		*_strcat(u8 *, u8 *);
 u8		**_strsplit(u8 *str, u8 *lim);
+
 void		print_char(char);
 void		print_string(char *);
 void		print_not_found(u8 *, u64, u8 *);
+
 void		free_string_array(u8 **);
-u8		*read_line();
+u8		*read_line(void);
+
 vector_t	*vector_new(vector_t *);
 vector_t	*vector_free(vector_t *);
 vector_t	*vector_write(vector_t *, void *src, u64 len);
 vector_t	*vector_read(vector_t *, void *dst, u64 size);
 u8		*vector_consume(vector_t *);
+
 set_t		*set_new(set_t *);
 set_t		*set_free(set_t *);
 u8		**set_consume(set_t *);
@@ -84,6 +89,7 @@ u8		set_filter_path(set_t *, u8 *);
 vector_t	*set_apply_path(set_t *, vector_t *);
 u8		set_filter_path_exec(set_t *, u8 *);
 vector_t	*set_apply_path_exec(set_t *, vector_t *);
+
 shell_t		*shell_new(shell_t *, u8 *name, set_t *envp, set_t *path);
 shell_t		*shell_init_return(shell_t *s, set_t *envp, set_t *path);
 shell_t		*shell_init(shell_t *, u8 *name, char **env);
@@ -92,16 +98,18 @@ shell_t		*shell_prompt(shell_t *);
 shell_t		*shell_exec(shell_t *, u8 *path, u8 **args);
 shell_t		*shell_exit(shell_t *, u8 nl);
 shell_t		*shell_exit_cmd(shell_t *, u8 **args);
+shell_t		*shell_setenv_cmd(shell_t *, u8 **args);
+shell_t		*shell_unsetenv_cmd(shell_t *, u8 **args);
 shell_t		*shell_iter_line(shell_t *, u8 **args, u64 line);
 shell_t		*shell_iter(shell_t *);
 shell_t		*shell_runtime(shell_t *);
 
-int hsh_execute(char **args);
-int hsh_exit(char **args);
-int launch_process(char **args);
+int			hsh_execute(char **args);
+int			hsh_exit(char **args);
+int			launch_process(char **args);
 
-void sigint_handler(int sig);
-void shell_set_signal(void);
+void		sigint_handler(int sig);
+void		shell_set_signal(void);
 
 # define BUFFER_SIZE (1024)
 # define EXIT_TEXT   "👋"
@@ -110,3 +118,4 @@ void shell_set_signal(void);
 # define ERRFOUND    "not found\n"
 
 #endif
+
